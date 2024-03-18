6eed87538063bc90bf3af0ee6e02cda04339ba02
Aryan20apr <aryansingh20nov@gmail.com>
2024-03-18 11:44:55 +0530

    Setting and fixing responsive footer using @HostListener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/layouts/header/header.component';
 import { FooterComponent } from './shared/layouts/footer/footer.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent],
+  imports: [NgStyle,RouterOutlet,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ecommerce';
+  screenHeight:any;
+  screenWidth:any;
+  footerMaxHeight:any;
+  
+  constructor()
+  {
+    this.getScreenSize(event);
+  }
+
+  @HostListener('window:resize',['$event'])
+  getScreenSize(event:any)
+  {
+    this.screenHeight=window.innerHeight;
+    this.screenWidth=window.innerWidth;
+    this.footerMaxHeight=this.screenHeight-160;
+  }
 }
